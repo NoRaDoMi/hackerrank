f7c8100b740c50cfa21cda473c480ff2c26ded37
Joe Karayusuf <jkarayusuf@gmail.com>
2013-06-04 23:19:26 -0700

    Ensure the dirty cells are found

--- bot_clean/test/grid.js
+++ bot_clean/test/grid.js
@@ -14,6 +14,14 @@ describe("Grid", function() {
     ]);
 
     assert.equal( 7, grid.dirtyCells.length );
+
+    assert.equal( "0 4", grid.dirtyCells[0].position() );
+    assert.equal( "1 1", grid.dirtyCells[1].position() );
+    assert.equal( "1 4", grid.dirtyCells[2].position() );
+    assert.equal( "2 2", grid.dirtyCells[3].position() );
+    assert.equal( "2 3", grid.dirtyCells[4].position() );
+    assert.equal( "3 2", grid.dirtyCells[5].position() );
+    assert.equal( "4 4", grid.dirtyCells[6].position() );
   });
 });
 
