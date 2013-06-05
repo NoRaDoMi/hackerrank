125819c2a38066db99171b8b6fdc3bf6bcf7de7a
Joe Karayusuf <jkarayusuf@gmail.com>
2013-06-04 22:24:16 -0700

    Add dirtyCell#distanceFrom

--- bot_clean/lib/dirty_cell.js
+++ bot_clean/lib/dirty_cell.js
@@ -3,4 +3,13 @@ function DirtyCell(x, y) {
   this.y = y;
 }
 
+DirtyCell.prototype = {
+  'distanceFrom' : function( cell ) {
+    var deltaX = Math.pow( this.x - cell.x, 2 )
+      , deltaY = Math.pow( this.y - cell.y, 2 );
+
+    return Math.sqrt( deltaX + deltaY );
+  }
+}
+
 module.exports = DirtyCell
--- bot_clean/test/dirty_cell.js
+++ bot_clean/test/dirty_cell.js
@@ -1,5 +1,6 @@
-var DirtyCell = require("../lib/dirty_cell.js");
-var assert = require("assert");
+var DirtyCell = require("../lib/dirty_cell.js")
+  , Bot = require("../lib/bot.js")
+  , assert = require("assert");
 
 describe("DirtyCell", function() {
 
@@ -8,6 +9,15 @@ describe("DirtyCell", function() {
 
     assert.equal( 4, dirtyCell.x );
     assert.equal( 2, dirtyCell.y );
+  });
+
+  describe("distanceFrom", function() {
+    it("returns the delta of the co-ordinates", function() {
+      var dirtyCell = new DirtyCell(-2, 1)
+        , bot = new Bot("1 5");
+
+      assert.equal( 5, dirtyCell.distanceFrom( bot ) );
+    });
   });
 
 });
